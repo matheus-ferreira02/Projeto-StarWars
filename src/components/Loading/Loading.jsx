dd473c7a3cfcbe80fef6636d6557b060649c3861
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-02-27 14:35:11 -0300

    style loading done

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import './style.css';
 function Loading() {
   return (
     <div className="loading-container">
-      <img className="loading-icon" src="https://conteudo.saraivaconecta.com.br/assets/img/ajax-loader-400.gif" alt="loading-gif" />
+      <div className="loading" />
     </div>
   );
 }
